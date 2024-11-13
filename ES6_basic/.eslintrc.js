2f3e3cdb3fe6e4d0ba0dc0c8c954c3c4eec16098
intex <jerrymeletiche123@gmail.com>
2024-11-13 12:48:07 -0400

    >>>

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['airbnb-base'], // Extiende la configuración de Airbnb
+  env: {
+    browser: true, // Si estás trabajando en un entorno de navegador
+    node: true, // Si también estás usando Node.js
+  },
+  rules: {
+    // Aquí puedes añadir reglas personalizadas, si es necesario
+  },
+};
