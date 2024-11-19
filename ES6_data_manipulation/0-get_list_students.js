1a8fcf54903fb3e96cddc6be98ed7c35bf259004
intex <jerrymeletiche123@gmail.com>
2024-11-19 09:59:20 -0400

    >>>

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,5 @@
 export default function getListStudents() {
-  const listStudents = []
+  const listStudents = [];
   listStudents.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
   listStudents.push({ id: 2, firstName: 'James', location: 'Columbia' });
   listStudents.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
