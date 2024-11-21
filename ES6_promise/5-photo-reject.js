0e85e10147af22cf0a0edb9284081a724ccec095
JerryEnClavE <jerrymeletiche123@gmail.com>
2024-11-21 15:15:42 -0400

    >>>

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
-export default function uploadPhoto(firstName) {
+export default function uploadPhoto(fileName) {
   return new Promise((resolve, reject) => {
-    reject(new Error('${filname} cannot be proccessed'));
+    reject(new Error(`${fileName} cannot be proccessed`));
   });
 }
