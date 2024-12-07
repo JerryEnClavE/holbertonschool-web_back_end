b339626f802f7b9061d648ebd0380bca37bdcefa
JerryEnClavE <jerrymeletiche123@gmail.com>
2024-12-06 23:55:40 -0400

    >>>

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+This module provides a type-annotated function `add` that
+takes two float arguments and returns their sum as a float.
+"""
+
+
 def add(a: float, b: float) -> float:
     """Returns the sum of two float numbers."""
     return a + b
