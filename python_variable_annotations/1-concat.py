c8a0d9a4331212f5a04ed9053f0f7094eb255265
JerryEnClavE <jerrymeletiche123@gmail.com>
2024-12-07 12:19:19 -0400

    >>>

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def concat(a: str, b: str) -> str:
+    
+    """
+    Concatenates two strings.
+
+    Parameters:
+    a (str): The first string to concatenate.
+    b (str): The second string to concatenate.
+
+    Returns:
+    str: The concatenated result of a and b.
+    """
+    
+    return a + b
