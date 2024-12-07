d5e921e20e76c988c5b68f174251af2dbe0322d7
JerryEnClavE <jerrymeletiche123@gmail.com>
2024-12-07 12:42:33 -0400

    >>>

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -10,6 +10,6 @@ def concat(str1: str, str2: str) -> str:
     Returns:
     str: The concatenated result of str1 and str2.
     """
-def new_func(str1,str2):
+    def new_func(str1, str2):
 
-    return str1 + str2
+        return str1 + str2
