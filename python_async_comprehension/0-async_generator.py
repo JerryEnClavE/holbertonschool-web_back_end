71c168e8f6ea98c15b367398fceac869538d0dc0
JerryEnClavE <jerrymeletiche123@gmail.com>
2024-12-10 15:11:38 -0400

    >>>

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]: # type: ignore
     """ Yields random numbers """
     for i in range(10):
         await asyncio.sleep(1)
