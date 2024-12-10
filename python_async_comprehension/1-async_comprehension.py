042de76ee5d262e0ed30f210d682a7cd219580a2
JerryEnClavE <jerrymeletiche123@gmail.com>
2024-12-10 17:43:04 -0400

    >>>

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python3
-"""
-This module contains an example of an async comprehension.
-"""
-
-import asyncio
-import random
-from typing import AsyncIterator, List
+""" File that contains the function async comprehension """
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[int]:
-    """
-    Coroutine collects numbers using an async comprehension.
-
-    Returns:
-        List[int]: List of 10 random integers.
-    """
-    numbers = [num async for num in async_generator()]
-    return numbers
+async def async_comprehension() -> List[float]:
+    """ Function that returns a list of randomly generated floats """
+    return [number async for number in async_generator()]
