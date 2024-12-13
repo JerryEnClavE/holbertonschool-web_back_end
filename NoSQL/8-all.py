93614ad4b7d4f2eb6596735fd798b8d2c0791d7f
JerryEnClavE <jerrymeletiche123@gmail.com>
2024-12-12 21:50:25 -0400

    >>>

---
+++ NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Return a list of all documents in a collection
+"""
+
+def list_all(mongo_colletion):
+    """Return a list of all documents in a collection"""
+    documents = []
+    for doc in mongo_colletion.find({}):
+        documents.append(doc)
+        return documents
